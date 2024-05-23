fa71e8d0843070f513a4ce980b22c9a6c78fc60e
ProgramandoComAndre <andrerafael112@gmail.com>
2024-05-23 16:13:41 +0100

    added viewBinding

--- app/src/main/java/pt/programandocomandre/contactagenda/activities/MainActivity.kt
+++ app/src/main/java/pt/programandocomandre/contactagenda/activities/MainActivity.kt
@@ -3,12 +3,27 @@ package pt.programandocomandre.contactagenda.activities
 import android.os.Bundle
 
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.lifecycleScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import pt.programandocomandre.contactagenda.ContactsDatabase
 import pt.programandocomandre.contactagenda.R
+import pt.programandocomandre.contactagenda.daos.ContactDao
+import pt.programandocomandre.contactagenda.databinding.ActivityMainBinding
+import pt.programandocomandre.contactagenda.models.Contact
 
 class MainActivity : AppCompatActivity() {
+
+    val viewBinding by lazy { ActivityMainBinding.inflate(layoutInflater) }
+    val contactDao by lazy { ContactsDatabase.getDatabase(applicationContext).contactsDao() }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(viewBinding.root)
+
+
+        lifecycleScope.launch(Dispatchers.IO) {
+            contactDao.insertContact(Contact(0, "Andre", "andrerafael112@gmail.com", "123456789"))
+        }
 
     }
 }
